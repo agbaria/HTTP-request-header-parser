734afadc042b628aa8b05bcbda1c3b29210cc100
Alaa Agbaria <agbaria.3laa@gmail.com>
2017-02-15 16:23:05 +0000

    final commit

--- index.js
+++ index.js
@@ -2,8 +2,25 @@ var express = require("express");
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send('hello world');
+    var header = req.headers;
+    var ans = {"ipaddress": null, 'language': null, 'software': null};
+    
+    ans.ipaddress = header['x-forwarded-for'];
+    ans.language = getLanguage(header['accept-language']);
+    ans.software = getSoftware(header['user-agent']);
+    
+    res.send(ans);
 });
+
+function getLanguage(lanstr) {
+    return lanstr ? lanstr.slice(0, lanstr.indexOf(',')) : null;
+}
+
+function getSoftware(softstr) {
+    return softstr ? 
+            softstr.slice(softstr.indexOf('(') + 1, softstr.indexOf(')')) : 
+            null;
+}
 
 app.listen(8080, function() {
     console.log('express app listening on port 8080!');
